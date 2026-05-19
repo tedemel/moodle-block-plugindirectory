0673c9e6add4fd35cbd3f808e147d72ec5464947
Tessa Demel <254993102+tedemel@users.noreply.github.com>
2026-05-19 21:06:56 +0200

    chore: promote to STABLE and declare Moodle 5.2 compatibility
    - Promote $plugin->maturity from MATURITY_ALPHA to MATURITY_STABLE
    - Set $plugin->supported = [405, 502]
    - Bump version 2026051100 -> 2026051901
    - Bump release 1.0.0 -> 1.0.1

--- version.php
+++ version.php
@@ -25,7 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_plugindirectory';
-$plugin->version   = 2026051100;
-$plugin->requires  = 2024100700; // Moodle 5.0.
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '1.0.0';
+$plugin->version   = 2026051901;
+$plugin->requires  = 2024100700; // Moodle 4.5 LTS.
+$plugin->supported = [405, 502];
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0.1';
